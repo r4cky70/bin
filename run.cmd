2ccda2391e5ef39e529b3e072c9944d4cd4f1438
r4cky70 <rodrigo.estrela.sousa@ccc.ufcg.edu.br>
2019-09-13 01:13:02 -0300

    ^^

--- run.cmd
+++ run.cmd
@@ -4,11 +4,13 @@ echo Compilling %1.cpp...
 IF [%2] == [] (
   g++ %1.cpp -std=c++14 -o sla
 ) ELSE (   
-  g++ %1.cpp -std=c++14 -DLOCAL -o sla )
+  g++ %1.cpp -std=c++14 -g -DLOCAL -o sla
+)
 echo Compilled.
 
 IF [%2] == [1] ( 
-  sla >out.txt
+  gdb -q -batch -x C:\Users\ESRELA\Documents\CProgramming\bin\threads.txt sla >out.txt
+
 ) ELSE (
-  sla
+  gdb -q -batch -x C:\Users\ESRELA\Documents\CProgramming\bin\threads.txt sla
 )
