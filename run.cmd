e0afdaa3d557d8f2e6eba367d3eed188a33a250a
r4cky70 <rodrigo.estrela.sousa@ccc.ufcg.edu.br>
2019-09-26 19:41:53 -0300

    update unsigned

--- run.cmd
+++ run.cmd
@@ -2,10 +2,11 @@
 
 echo Compilling %1.cpp...
 IF [%2] == [] (
-  g++ %1.cpp -std=c++14 -Wall -Wextra -Wfatal-errors -Wl,--stack=268435456 -o sla
+  g++ %1.cpp -std=c++14 -Wall -Wextra -Wfatal-errors -Wl,--stack=268435456 -Wno-sign-compare -o sla
 ) ELSE (   
-  g++ %1.cpp -std=c++14 -Wall -Wextra -Wfatal-errors -Wl,--stack=268435456 -DLOCAL -o sla
+  g++ %1.cpp -std=c++14 -Wall -Wextra -Wfatal-errors -Wl,--stack=268435456 -Wno-sign-compare -DLOCAL -o sla
 )
+REM -fsanitize=undefined
 echo Compilled.
 
 IF [%2] == [1] ( 
